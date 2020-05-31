6a5de9ed790977e47adbe81a946d4923541d87e2
ftnext <takuyafjp+develop@gmail.com>
2020-05-31 13:19:47 +0900

    [chore] add setup.py to pip install thie repo (to create lambda layer)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="operate_drive",
+    version="0.1",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    install_requires=["PyDrive2"],
+)
