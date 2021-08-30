f8d1042c6516b6a24a7c11f5fba892e39a868f79
nikkie <takuyafjp+develop@gmail.com>
2021-08-31 01:27:09 +0900

    [chore] Bump up with #4, #6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="operate_drive",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(exclude=["tests.*", "tests"]),
     install_requires=["PyDrive2"],
 )
